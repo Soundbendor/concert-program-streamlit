489a8aa05439519b7f78d8d3f64209acb01cafec
Jonah Broyer <jkbsk8er@yahoo.com>
2023-01-23 15:01:46 -0800

    replaced call to container for header test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-header = st.beta_container()
-dataset = st.beta_container()
-features = st.beta_container()
+header = st.container()
+dataset = st.container()
+features = st.container()
 
 with header:
-    st.Title('soundbendor lab')
+    st.title('soundbendor lab')
