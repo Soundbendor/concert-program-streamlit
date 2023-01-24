9c3c75ca047b73d23786c07ff92723d7f01522f4
Jonah Broyer <jkbsk8er@yahoo.com>
2023-01-23 16:34:56 -0800

    add list of example composers

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,11 @@ with header:
     st.text('Researchers in computational musicology desire to apply modern natural language processing techniques to historic documents describing musical pieces and specific performances. This project allows users to download datasets of professional symphony orchestras concert programs.')
 
 with dataset:
-    st.header('Concert Program dataset')
+    st.header('concert program dataset')
     st.text('This data is scrapped from the New York Philharmonic which provides publicily available archives of documents.')
 
 with features:
-    st.header('Available composers')
+    st.header('available composers')
+    st.markdown('* **Beethoven**')
+    st.markdown('* **Mozart**')
+    st.markdown('* **Tchaikovsky**')
