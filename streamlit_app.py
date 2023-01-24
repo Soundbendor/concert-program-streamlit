26eb690b578b5c35ae9aac8ee36ef399d8b6cfd5
Jonah Broyer <jkbsk8er@yahoo.com>
2023-01-23 16:26:31 -0800

    added descriptive text

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,12 @@ dataset = st.container()
 features = st.container()
 
 with header:
-    st.title('soundbendor lab')
+    st.title('soundbendor lab: concert program OCR')
+    st.text('Researchers in computational musicology desire to apply modern natural language processing techniques to historic documents describing musical pieces and specific performances. This project allows users to download datasets of professional symphony orchestras concert programs.')
+
+with dataset:
+    st.header('Concert Program dataset')
+    st.text('This data is scrapped from the New York Philharmonic which provides publicily available archives of documents.')
+
+with features:
+    st.header('Available composers')
